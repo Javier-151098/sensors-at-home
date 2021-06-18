1fc4d8eb71a080914fb463fedaa63a9f8022e501
gloriaalejandra <gloriaalejandra967@gmail.com>
2021-06-18 01:17:11 -0500

    18/06/2021.Javier

--- ProyectoAndroidStudio/app/src/main/java/com/example/sweetathome/mainPrincipal.java
+++ ProyectoAndroidStudio/app/src/main/java/com/example/sweetathome/mainPrincipal.java
@@ -9,6 +9,6 @@ public class mainPrincipal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ventana_principal);
+        setContentView(R.layout.activity_main);
     }
 }
