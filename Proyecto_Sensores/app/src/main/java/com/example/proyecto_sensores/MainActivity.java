05812c736ed40066407a4dde461d3304f46eda1b
Javier-151098 <porcientopron@gmail.com>
2021-06-25 23:14:23 -0500

    25/06/2021.J

--- Proyecto_Sensores/app/src/main/java/com/example/proyecto_sensores/MainActivity.java
+++ Proyecto_Sensores/app/src/main/java/com/example/proyecto_sensores/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pantalla_principal_menu_lateral);
         //Se modifica para ejecutar la ventana de inicio
 
     }
